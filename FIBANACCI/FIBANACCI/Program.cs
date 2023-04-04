700eb96d3f44518e69c58887088fa328efd292f1
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-04 19:47:54 +0300

    FIBANACCI

---
+++ FIBANACCI/FIBANACCI/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FIBANACCI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = 0, secondnumber = 1, threenumber = 3, i, number1;
+            Console.WriteLine(" enter your number ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write(firstnumber + " " + secondnumber);
+            for (i = 3; i <= number; i++)
+            {
+                threenumber = firstnumber + secondnumber;
+                Console.Write(threenumber + " ");
+                firstnumber = secondnumber;
+                secondnumber = threenumber;
+            }
+
+
+            Main(args);
+        }
+    }
+}
