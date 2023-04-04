f685d23eaabf7a4ccbf51ccfaa26a5ff6ff231fb
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-04 19:55:11 +0300

    BMI

---
+++ BMI/BMI/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter your weight ");
+            double Weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter your height");
+            float Height = float.Parse(Console.ReadLine());
+            float metres = Height / 100;
+            float kvadrat = metres * metres;
+            Console.Write("BMI ==> ");
+            Console.WriteLine(Weight / kvadrat);
+            Main(args);
+        }
+    }
+}
