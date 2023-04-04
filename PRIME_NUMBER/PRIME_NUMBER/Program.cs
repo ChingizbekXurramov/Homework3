5c1c5edc11b7cb366ba4a5ded5bb4f066683d50f
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-04 19:51:54 +0300

    PRIME_NUMBER

---
+++ PRIME_NUMBER/PRIME_NUMBER/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PRIME_NUMBER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prime = true;
+            Console.WriteLine(" enter your number ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime)
+            {
+                Console.WriteLine("IS A PRIME NUMBER");
+            }
+            else
+            {
+                Console.WriteLine("IS NOT A PRIME NUMBER");
+            }
+
+            Main(args);
+        }
+    }
+}
